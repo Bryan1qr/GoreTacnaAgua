5434cb17f39524078f0423faf4e88c69acd52c9b
Bryan1qr <bquisper@unjbg.edu.pe>
2025-04-10 16:46:28 -0500

    v52025

--- Ejecución/Final_test.R
+++ Ejecución/Final_test.R
@@ -9,7 +9,21 @@ df1 <- dataset_agua2(
   ruta = "data", 
   coordenadas = "spatial_input")
 
-df2 <- df1 %>% select(codigo, cuenca, descripcion, zona, este, norte, categoria, tipo, cuerpo_agua) %>% 
+actividad <- df1 %>% mutate(estado = if_else(fecha_larga >= "2023-01-01", "activo", "inactivo")) %>%
+  filter(estado == "activo") %>% select(cuenca, codigo) %>% distinct()
+
+df3 <- df1 %>% left_join(actividad %>% select(codigo, cuenca) %>% mutate(estado = "activo"),
+                 by = c("codigo", "cuenca")) %>%
+  mutate(estado = replace_na(estado, "inactivo"),
+         categoria = case_when(
+           categoria == "C4" ~ "C4E2",
+           categoria == "C3" ~ "C3D1",
+           TRUE ~ categoria
+         ))
+
+
+########### ESPACIAL #################################
+df2 <- df3 %>% select(codigo, cuenca, descripcion, zona, este, norte, categoria, tipo, cuerpo_agua) %>% 
   distinct() %>% 
   st_as_sf(coords = c("este", "norte"), crs = 32719) %>%
   st_transform(crs = 4326) %>%            
@@ -19,14 +33,6 @@ df2 <- df1 %>% select(codigo, cuenca, descripcion, zona, este, norte, categoria,
   ) %>% st_drop_geometry() %>% 
   as.data.frame() %>% mutate(coordenadas = paste0(lat, ", ", lon)) %>% 
   select(-lon, -lat)
-  
-actividad <- df1 %>% mutate(estado = if_else(fecha_larga >= "2023-01-01", "activo", "inactivo")) %>%
-  filter(estado == "activo") %>% select(cuenca, codigo) %>% distinct()
-
-df3 <- df1 %>% left_join(actividad %>% select(codigo, cuenca) %>% mutate(estado = "activo"),
-                 by = c("codigo", "cuenca")) %>%
-  mutate(estado = replace_na(estado, "inactivo"))
-
 
 df4 <- df2 %>% 
   left_join(actividad %>% select(codigo, cuenca) %>% mutate(estado = "activo"),
@@ -52,3 +58,38 @@ duplicados <- df3 %>%
   group_by(id, PARAMETROS) %>%  # Agrupa por id y parámetros
   count() %>%  # Cuenta cuántos registros existen para cada grupo
   filter(n > 1)
+
+# Actualizar el google sheet: ---------------------------------------------
+sheet_id <- drive_find(pattern = "dataset_agua_v2") %>% pull(id)
+
+range_write(sheet_id, range = "Hoja1", data = df3, reformat = TRUE)
+
+
+
+# Actualizar el google sheet2: ---------------------------------------------
+sheet_id2 <- drive_find(pattern = "dataset_espacial_v3") %>% pull(id)
+
+range_write(sheet_id2, range = "Hoja1", data = df4, reformat = TRUE)
+
+# Tablas largas -----------------------------------------------------------
+
+dataset_largo_v2 <- df3 %>% mutate(id = paste(cuenca, codigo, fecha_larga, sep = "-")) %>% 
+  pivot_wider(names_from = PARAMETROS, values_from = valor, 
+              id_cols = c(cuenca, codigo, fecha_larga, tipo, cuerpo_agua, estado, categoria, zona, este, norte, descripcion)) %>% 
+  arrange(cuenca, codigo, fecha_larga)
+
+gs4_create("dataset_agua_largo_v1", sheets = list("Hoja1" = dataset_largo_v2))
+
+# Actualizar el google sheet: ---------------------------------------------
+sheet_id3 <- drive_find(pattern = "dataset_agua_largo_v1") %>% pull(id)
+
+range_write(sheet_id3, range = "Hoja1", data = dataset_largo_v2, reformat = TRUE)
+
+
+dfx <- df3 %>% mutate(id = paste(cuenca, codigo, fecha_larga, sep = "-"))
+
+dfx %>% dplyr::summarise(n = dplyr::n(), .by = c(id, PARAMETROS)) |>
+  dplyr::filter(n > 1L) %>% View()
+
+
+openxlsx::write.xlsx(dataset_largo_v2, "tabla_larga_agua_tacna.xlsx")
